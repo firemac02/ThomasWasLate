18dbf0011070476980cc021b4daa7e617db95d07
Morgan Orr <A0076639@ayrshire.ac.uk>
2022-01-20 10:57:04 +0000

    did stuff

---
+++ ThomasWasLate/ParticleSystem.h
@@ -0,0 +1,24 @@
+#ifndef PARTICLESYSTEM_H
+#define PARTICLESYSTEM_H
+
+#include "Particle.h"
+using namespace sf;
+using namespace std;
+
+class ParticleSystem : public Drawable
+{
+private:
+	vector<Particle> m_Particles;
+	VertexArray m_Vertices;
+	float m_Duration;
+	bool m_IsRunning = false;
+public:
+	virtual void draw(RenderTarget& target, RenderStates states) const;
+
+	void init(int count);
+	void emitParticles(Vector2f position);
+	void update(float dt);
+	bool running();
+};
+
+#endif PARTICLESYSTEM_H
